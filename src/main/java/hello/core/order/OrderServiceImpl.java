9cb9c5ededca36d00ee86302b20beadc20f70c03
0chil <0@chll.it>
2022-01-30 23:42:20 +0900

    add: introduce Lombok

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -6,18 +6,20 @@ import org.springframework.stereotype.Component;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
 	private final MemberRepository memberRepository;
 	private final DiscountPolicy discountPolicy;
 
-	@Autowired // 생성자가 유일한 경우에는 생략해도 같은 효과
-	public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-		this.memberRepository = memberRepository;
-		this.discountPolicy = discountPolicy;
-	}
+	// @Autowired // 생성자가 유일한 경우에는 생략해도 같은 효과
+	// public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+	// 	this.memberRepository = memberRepository;
+	// 	this.discountPolicy = discountPolicy;
+	// }
 
 	@Override
 	public Order createOrder(Long memberId, String itemName, int itemPrice) {
